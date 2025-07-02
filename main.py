235c36d923ff7f713ba45b4f4a6aa93976d46e08
arian-ador <arianador77@gmail.com>
2025-07-02 12:00:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,6 @@
 #  |    |   \ / __ \|  |  |  | |  |_\  ___/ \___ \|   |  \/ __ \|    <\  ___/
 #  |________/(______/__|  |__| |____/\_____>______>___|__(______/__|__\\_____>
 #
-# This file can be a nice home for your Battlesnake logic and helper functions.
-#
-# To get you started we've included code to prevent your Battlesnake from moving backwards.
-# For more info see docs.battlesnake.com
-
 import random
 import typing
 
@@ -22,10 +17,10 @@ def info() -> typing.Dict:
 
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "author": "Naughtysnake",  # TODO: Your Battlesnake Username
+        "color": "#66b3ff",  # TODO: Choose color
+        "head": "mlh-gene",  # TODO: Choose head
+        "tail": "ice-skate",  # TODO: Choose tail
     }
 
 
@@ -41,7 +36,6 @@ def end(game_state: typing.Dict):
 
 # move is called on every turn and returns your next move
 # Valid moves are "up", "down", "left", or "right"
-# See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
 
     is_move_safe = {"up": True, "down": True, "left": True, "right": True}
@@ -49,7 +43,20 @@ def move(game_state: typing.Dict) -> typing.Dict:
     # We've included code to prevent your Battlesnake from moving backwards
     my_head = game_state["you"]["body"][0]  # Coordinates of your head
     my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
-
+    board_width = game_state['board']['width'] 
+    board_height = game_state['board']['height'] 
+    my_body = game_state['you']['body'] 
+    opponents = game_state['board']['snakes'] 
+    food_list = game_state["board"]["food"] # List of Food coordinates
+    
+    direction = { # Directions are based on coordinates of x and y
+        "up": [my_head['x'], my_head['y'] + 1],
+        "down": [my_head['x'], my_head['y'] - 1],
+        "left": [my_head['x'] -1, my_head['y']],
+        "right": [my_head['x'] + 1, my_head['y']]
+    }
+    safe_moves = []
+    
     if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
         is_move_safe["left"] = False
 
@@ -62,31 +69,66 @@ def move(game_state: typing.Dict) -> typing.Dict:
     elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
         is_move_safe["up"] = False
 
-    # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
+    
+    # Step 1 - Prevent your Battlesnake from moving out of bounds
+    for move, pos in direction.items():
+        x, y = pos[0], pos[1]
+        new_pos = {"x": x, "y": y}
+        # Check the Walls
+        if not (0 <= x < board_width and 0 <= y < board_height):
+            continue 
+    # Step 2 - Prevent your Battlesnake from colliding with itself
+        if new_pos in my_body:
+            continue             
+    # Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
+        collision = False
+        for snake in opponents:
+            if new_pos in snake['body']:
+                collision = True
+                break
+        if collision:
+            continue  # collision detected
 
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
+        safe_moves.append(move) # move is safe
+        
 
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
+    if not safe_moves: # No safe moves, go down
+        next_move = "down"
+    else:
+        if food_list: # If there is food on the board
+            def manhattan_dist(p1, p2): 
+                return abs(p1["x"] - p2["x"]) + abs(p1["y"] - p2["y"])  
+                # Determine the direction to the nearest food   
+        
+            nearest_food = min(food_list, key=lambda f:manhattan_dist(my_head, f)) 
+            # Find the move that brings the snake closest to the nearest food
+            best_move = None 
+            min_dist = float('inf')
+            for move in safe_moves: 
+                new_x, new_y = direction[move]  # Get the new position after the move
+                dist = abs(new_x - nearest_food["x"]) + abs(new_y - nearest_food["y"]) # Calculate the distance to the nearest food
+                if dist < min_dist: 
+                    min_dist = dist
+                    best_move = move
+            next_move = best_move
+        else:
+            # Calculate the score of the move based on the space around it
+            def space_score(move):  
+                x, y = direction[move]  
+                score = 0 
+                for dx in [-1, 0, 1]:  
+                    for dy in [-1, 0, 1]: 
+                        if dx == 0 and dy == 0:
+                            continue  # Skip the current cell
+                            # Check if the cell is within the board boundaries
+                        if 0 <= x + dx < board_width and 0 <= y + dy < board_height: 
+                            if{"x": x + dx, "y": y + dy} not in my_body: # Check if the cell is empty
+                                score += 1
+                    return score
 
-    # Are there any safe moves left?
-    safe_moves = []
-    for move, isSafe in is_move_safe.items():
-        if isSafe:
-            safe_moves.append(move)
-
-    if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
-        return {"move": "down"}
-
-    # Choose a random move from the safe ones
-    next_move = random.choice(safe_moves)
-
-    # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
-    # food = game_state['board']['food']
+            next_move = max(safe_moves, key=space_score) # Choose the move with the highest score
+            
+        
 
     print(f"MOVE {game_state['turn']}: {next_move}")
     return {"move": next_move}
